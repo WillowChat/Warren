03d81a26b67d5d67444df5673c5c45d6f24d6f23
Sky Welch <git@bunnies.io>
2016-04-23 15:48:42 +0100

    MOTD updates lifecycle to connected

--- src/main/kotlin/engineer/carrot/warren/warren/handler/Rpl376Handler.kt
+++ src/main/kotlin/engineer/carrot/warren/warren/handler/Rpl376Handler.kt
@@ -4,12 +4,15 @@ import engineer.carrot.warren.kale.IKaleHandler
 import engineer.carrot.warren.kale.irc.message.rfc1459.JoinMessage
 import engineer.carrot.warren.kale.irc.message.rpl.Rpl376Message
 import engineer.carrot.warren.warren.IMessageSink
+import engineer.carrot.warren.warren.state.ConnectionState
+import engineer.carrot.warren.warren.state.LifecycleState
 
-class Rpl376Handler(val sink: IMessageSink, val channelsToJoin: List<String>) : IKaleHandler<Rpl376Message> {
+class Rpl376Handler(val sink: IMessageSink, val channelsToJoin: List<String>, val connectionState: ConnectionState) : IKaleHandler<Rpl376Message> {
     override val messageType = Rpl376Message::class.java
 
     override fun handle(message: Rpl376Message) {
-        println("got end of MOTD, joining channels")
+        println("got end of MOTD, updating lifecycle to CONNECTED and joining channels")
+        connectionState.lifecycle = LifecycleState.CONNECTED
         sink.write(JoinMessage(channels = channelsToJoin))
     }
 }
--- src/test/kotlin/engineer/carrot/warren/warren/handler/Rpl376HandlerTests.kt
+++ src/test/kotlin/engineer/carrot/warren/warren/handler/Rpl376HandlerTests.kt
@@ -5,21 +5,34 @@ import com.nhaarman.mockito_kotlin.verify
 import engineer.carrot.warren.kale.irc.message.rfc1459.JoinMessage
 import engineer.carrot.warren.kale.irc.message.rpl.Rpl376Message
 import engineer.carrot.warren.warren.IMessageSink
+import engineer.carrot.warren.warren.state.ConnectionState
+import engineer.carrot.warren.warren.state.LifecycleState
 import org.junit.Before
 import org.junit.Test
+import org.junit.Assert.assertEquals
 
 class Rpl376HandlerTests {
     lateinit var handler: Rpl376Handler
     lateinit var mockSink: IMessageSink
+    lateinit var connectionState: ConnectionState
 
     @Before fun setUp() {
+        val lifecycleState = LifecycleState.CONNECTING
+        connectionState = ConnectionState(server = "test.server", port = 6697, nickname = "test-nick", username = "test-nick", lifecycle = lifecycleState)
+
         mockSink = mock()
-        handler = Rpl376Handler(mockSink, channelsToJoin = listOf("#channel1", "#channel2"))
+        handler = Rpl376Handler(mockSink, channelsToJoin = listOf("#channel1", "#channel2"), connectionState = connectionState)
     }
 
     @Test fun test_handle_SendsPongWithCorrectToken() {
         handler.handle(Rpl376Message(source = "test.source", target = "test-user", contents = "end of motd"))
 
         verify(mockSink).write(JoinMessage(channels = listOf("#channel1", "#channel2")))
+    }
+
+    @Test fun test_handle_UpdatesLifecycleStateToConnected() {
+        handler.handle(Rpl376Message(source = "test.source", target = "test-user", contents = "end of motd"))
+
+        assertEquals(LifecycleState.CONNECTED, connectionState.lifecycle)
     }
 }
