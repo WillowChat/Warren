63efd58657a5ee621d4f6a1761c53bc1ff0c82a1
Sky Welch <git@bunnies.io>
2016-04-23 15:49:16 +0100

    Update PART for lifecycle support

--- src/test/kotlin/engineer/carrot/warren/warren/handler/PartHandlerTests.kt
+++ src/test/kotlin/engineer/carrot/warren/warren/handler/PartHandlerTests.kt
@@ -1,15 +1,14 @@
 package engineer.carrot.warren.warren.handler
 
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.verify
 import engineer.carrot.warren.kale.irc.message.rfc1459.PartMessage
 import engineer.carrot.warren.kale.irc.prefix.Prefix
 import engineer.carrot.warren.warren.state.ChannelState
 import engineer.carrot.warren.warren.state.ChannelsState
 import engineer.carrot.warren.warren.state.ConnectionState
+import engineer.carrot.warren.warren.state.LifecycleState
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.junit.Assert.*
 
 class PartHandlerTests {
 
@@ -18,7 +17,8 @@ class PartHandlerTests {
     lateinit var channelsState: ChannelsState
 
     @Before fun setUp() {
-        connectionState = ConnectionState(server = "test.server", port = 6697, nickname = "test-nick", username = "test-user")
+        val lifecycleState = LifecycleState.DISCONNECTED
+        connectionState = ConnectionState(server = "test.server", port = 6697, nickname = "test-nick", username = "test-user", lifecycle = lifecycleState)
         channelsState = ChannelsState(joined = mutableMapOf())
         handler = PartHandler(connectionState, channelsState)
     }
