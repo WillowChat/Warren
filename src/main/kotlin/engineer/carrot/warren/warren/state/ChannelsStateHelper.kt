e48f8117b1d58c893fab42c48c76f755c8a29e40
Sky Welch <git@bunnies.io>
2016-04-30 21:26:02 +0100

    Helper functions for generates lists of users with modes

--- src/main/kotlin/engineer/carrot/warren/warren/state/ChannelsStateHelper.kt
+++ src/main/kotlin/engineer/carrot/warren/warren/state/ChannelsStateHelper.kt
@@ -10,6 +10,21 @@ fun generateUsers(vararg nicks: String): MutableMap<String, ChannelUserState> {
     return users
 }
 
+fun generateUsersWithModes(vararg nicks: Pair<String, Set<Char>>): MutableMap<String, ChannelUserState> {
+    val users = mutableMapOf<String, ChannelUserState>()
+
+    for ((nick, modes) in nicks) {
+        users += generateUser(nick, modes)
+    }
+
+    return users
+}
+
+
 fun generateUser(nick: String): Pair<String, ChannelUserState> {
     return (nick to ChannelUserState(nick))
+}
+
+fun generateUser(nick: String, modes: Set<Char>): Pair<String, ChannelUserState> {
+    return (nick to ChannelUserState(nick, modes))
 }
