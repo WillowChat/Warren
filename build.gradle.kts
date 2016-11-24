57d9ac2797ef590401fb0b951d8f465e7801f3f3
Sky Welch <git@bunnies.io>
2016-11-24 14:12:27 +0000

    Bump Kale version after CI changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,7 @@ repositories {
 dependencies {
     compile(kotlinModule("stdlib"))
     compile("org.slf4j:slf4j-api:1.7.21")
-    compile("engineer.carrot.warren.kale:Kale:1.2.0.124")
+    compile("engineer.carrot.warren.kale:Kale:1.2.0.2")
     compile("com.squareup.okio:okio:1.11.0")
 
     runtime("org.slf4j:slf4j-simple:1.7.21")
